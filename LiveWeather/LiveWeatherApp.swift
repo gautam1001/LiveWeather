a96fb56d46780e6bcef2f808ec78d9bcaca69152
Prashant Gautam <prashant.gtm1001@gmail.com>
2026-03-21 19:15:54 +0530

    Project file and packages added
    Basic setup files uploaded
    Data Domain and Presentation layer added as a package

---
+++ LiveWeather/LiveWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveWeatherApp.swift
+//  LiveWeather
+//
+//  Created by Prashant Gautam on 20/03/26.
+//
+
+import SwiftUI
+
+@main
+struct LiveWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
